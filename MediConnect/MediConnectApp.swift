611c7613370e81a83919d024cb54742cd4c9d14d
TheHoppinBunnies <thehoppinbunnies@gmail.com>
2025-03-12 15:54:04 -0400

    Initial Commit

---
+++ MediConnect/MediConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediConnectApp.swift
+//  MediConnect
+//
+//  Created by Othmane EL MARIKY on 2025-03-12.
+//
+
+import SwiftUI
+
+@main
+struct MediConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
